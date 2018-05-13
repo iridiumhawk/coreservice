f3e2faddcd3a849921587ed934e886d808c23c2a
iridiumhawk <hawk.tmn@gmail.com>
2018-05-13 23:42:33 +0300

    add Helper class; changes Configuration;

---
+++ src/main/java/com/cherkasov/utils/Helper.java
@@ -0,0 +1,20 @@
+package com.cherkasov.utils;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class Helper {
+    public static String getClientIp(HttpServletRequest request) {
+
+        String remoteAddr = "";
+
+        if (request != null) {
+            remoteAddr = request.getHeader("X-FORWARDED-FOR");
+            if (remoteAddr == null || "".equals(remoteAddr)) {
+                remoteAddr = request.getRemoteAddr();
+            }
+        }
+
+        return remoteAddr;
+    }
+
+}
