3843ea2b241a51a139e176dfb0d4ccddb0661107
Alexey Cherkasov <alexey.cherkasov@open.ru>
2018-05-17 15:24:25 +0300

    add parameters to mongo config

--- src/main/java/com/cherkasov/MongoConfig.java
+++ src/main/java/com/cherkasov/MongoConfig.java
@@ -3,6 +3,11 @@ package com.cherkasov;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -12,6 +17,14 @@ import java.util.Collection;
 @Configuration
 @EnableMongoRepositories
 public class MongoConfig extends AbstractMongoConfiguration {
+
+    private int CONNECTION_TIME_OUT_MS = 10000;
+    private int SOCKET_TIME_OUT_MS = 0;
+    private int SERVER_SELECTION_TIMEOUT_MS = 5000;
+
+    private String username = "admin";
+    private String password = "password";
+
     @Override
     protected String getDatabaseName() {
 
@@ -21,7 +34,22 @@ public class MongoConfig extends AbstractMongoConfiguration {
     @Override
     public Mongo mongo() throws Exception {
 
-        return new MongoClient("localhost", 27017);
+        List<MongoCredential> creds = new ArrayList<MongoCredential>();
+        creds.add(MongoCredential.createCredential(username, getDatabaseName(), password.toCharArray()));
+
+        MongoClientOptions.Builder optionsBuilder = MongoClientOptions.builder();
+
+        optionsBuilder.connectTimeout(CONNECTION_TIME_OUT_MS);
+
+        optionsBuilder.socketTimeout(SOCKET_TIME_OUT_MS);
+
+        optionsBuilder.serverSelectionTimeout(SERVER_SELECTION_TIMEOUT_MS);
+
+        MongoClientOptions options = optionsBuilder.build();
+
+        return new MongoClient(new ServerAddress("localhost" , 27017), options);
+
+//        return new MongoClient("localhost", 27017);
     }
 
     @Override
