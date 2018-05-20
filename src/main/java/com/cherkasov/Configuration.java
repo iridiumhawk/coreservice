3d5ad3cea1763ff85b2efbe7ccf23a154a8b174a
iridiumhawk <hawk.tmn@gmail.com>
2018-05-20 22:19:39 +0300

    update helper and configuration

--- src/main/java/com/cherkasov/Configuration.java
+++ src/main/java/com/cherkasov/Configuration.java
@@ -2,8 +2,9 @@ package com.cherkasov;
 
 
 public class Configuration {
-    public final static String clientURL = "http://*:8083/ZWaveAPI/Run/controller.data.uuid";
+    public final static String clientURL = "http://*:8083/ZWaveAPI/Run/controller.data.uuid.value";
     public final static String clientData = "http://*:8083/ZAutomation/api/v1/devices/";
+    public final static String deviceData = "http://*:8083/ZAutomation/api/v1/devices";
     public final static String clientLogin = "admin";
     public final static String clientPassword = "T4535nhsdf68nMNb54";
 
--- src/main/java/com/cherkasov/utils/Helper.java
+++ src/main/java/com/cherkasov/utils/Helper.java
@@ -1,6 +1,10 @@
 package com.cherkasov.utils;
 
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.http.HttpHeaders;
+
 import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.Charset;
 
 public class Helper {
     public static String getClientIp(HttpServletRequest request) {
@@ -15,6 +19,16 @@ public class Helper {
         }
 
         return remoteAddr;
+    }
+
+    public static HttpHeaders createHeaders(String username, String password) {
+
+        return new HttpHeaders() {{
+            String auth = username + ":" + password;
+            byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("UTF-8")));
+            String authHeader = "Basic " + new String(encodedAuth);
+            set("Authorization", authHeader);
+        }};
     }
 
 }
