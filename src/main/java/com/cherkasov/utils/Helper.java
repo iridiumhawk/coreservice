5ad7b9aae6dc4b58746c729244d5f560dcb832c5
iridiumhawk <hawk.tmn@gmail.com>
2018-06-18 23:38:30 +0300

    minor changes

--- src/main/java/com/cherkasov/utils/Helper.java
+++ src/main/java/com/cherkasov/utils/Helper.java
@@ -1,7 +1,10 @@
 package com.cherkasov.utils;
 
+import com.cherkasov.Configuration;
 import org.apache.commons.codec.binary.Base64;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpHeaders;
+import org.springframework.web.client.RestOperations;
 
 import javax.servlet.http.HttpServletRequest;
 import java.nio.charset.Charset;
@@ -46,6 +49,12 @@ public class Helper {
             return split[0];
         }
         return "";
+    }
+
+    @Deprecated
+    public RestOperations rest(RestTemplateBuilder restTemplateBuilder) {
+
+        return restTemplateBuilder.basicAuthorization(Configuration.clientLogin, Configuration.clientPassword).setConnectTimeout(5000).build();
     }
 
 }
