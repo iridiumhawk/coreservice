77dadd75f41e608d824f0bd29afce04983bacc05
iridiumhawk <hawk.tmn@gmail.com>
2018-10-01 01:08:46 +0300

    some fixes in Abstract DAO

--- src/main/java/com/cherkasov/repositories/AbstractDAO.java
+++ src/main/java/com/cherkasov/repositories/AbstractDAO.java
@@ -43,38 +43,20 @@ public abstract class AbstractDAO<T> {
         this.operations.save(value, collection);
     }
 
-    public WriteResult update(String fieldName, String fieldValue, T entity, String collection) {
+
+    // TODO: 27.09.2018 field _class is absent while JSON parsing
+    public T update(String fieldName, String fieldValue, T entity, String collection) {
         Gson gson = new Gson();
-        DBObject newEntity = BasicDBObject.parse(gson.toJson(entity));// TODO: 27.09.2018 field _class is absent on JSON parsing
-        log.info("DBObject: {}", newEntity);
+        DBObject newEntity = BasicDBObject.parse(gson.toJson(entity));
+        log.debug("DBObject: {}", newEntity);
         Update update = Update.fromDBObject(newEntity);
-        log.info("Update: {}", update);
+        log.debug("Update: {}", update);
 
         Query query = new Query(Criteria.where(fieldName).is(fieldValue).andOperator(Criteria.where("_class").is(getGenericType().getName())));
-         return this.operations.upsert(query, update, entity.getClass(), collection);
+         return (T)this.operations.findAndModify(query, update, entity.getClass(), collection);
+//       return this.operations.updateFirst(query, update, entity.getClass(), collection);
+//       return this.operations.save(entity, collection);
 
-//        this.operations.save(entity, collection);
-
-//        T byName = findByName(entity.getName(), collection);
-//        if (byName != null) {
-//            entity.setId(byName.getId());
-//        this.operations.findAndModify(query, update, entity.getClass());
-      /*Query query6 = new Query();
-		query6.addCriteria(Criteria.where("name").is("appleF"));
-
-		Update update6 = new Update();
-		update6.set("age", 101);
-		update6.set("ic", 1111);
-
-		//FindAndModifyOptions().returnNew(true) = newly updated document
-		//FindAndModifyOptions().returnNew(false) = old document (not update yet)
-		User userTest6 = mongoOperation.findAndModify(
-				query6, update6,
-				new FindAndModifyOptions().returnNew(true), User.class);*/
-//            this.operations.save(entity, collection);
-//        } else {
-//            insert(entity, collection);
-//        }
     }
 
     public void updateAll(String fieldName, String fieldValue, List<T> values, String collection) {
