55d714d9fffe7b28402fd1d27eb4de30351740a8
iridiumhawk <hawk.tmn@gmail.com>
2018-10-01 00:47:27 +0300

    change version

--- src/main/java/com/cherkasov/configuration/SwaggerConfig.java
+++ src/main/java/com/cherkasov/configuration/SwaggerConfig.java
@@ -43,7 +43,7 @@ public class SwaggerConfig {
 
     private ApiInfo apiInfo() {
         return new ApiInfo(
-                "Smart Home - REST API, v.0.4.1",
+                "Smart Home - REST API, v.0.6.0",
                 "REST API interface and documentation.",
                 null,
                 null,
