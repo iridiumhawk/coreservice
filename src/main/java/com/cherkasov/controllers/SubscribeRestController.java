aa7667c6791dc554c4fa5ed69f7ca1bed2f15f3f
Alexey Cherkasov <alexey.cherkasov@open.ru>
2018-10-01 17:52:02 +0300

    add method View in controller

--- src/main/java/com/cherkasov/controllers/SubscribeRestController.java
+++ src/main/java/com/cherkasov/controllers/SubscribeRestController.java
@@ -4,6 +4,7 @@ import com.cherkasov.entities.ClientSubscription;
 import com.cherkasov.services.SubscriptionService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/subscribe/{id}")
+@RequestMapping("/api/v1/subscribe")
 public class SubscribeRestController {
 
   @Autowired
@@ -19,20 +20,36 @@ public class SubscribeRestController {
 
     private final HttpStatus okStatus = HttpStatus.OK;
 
+  @ApiOperation(value = "Просмотреть подписки", notes = "Просмотреть все текущие подписки на  всех устройствах.", produces = "application/json", consumes = "application/json")
+  @RequestMapping(value = "/view", method = RequestMethod.GET)
+  public ResponseEntity<?> view(
+//            @ApiParam(value = "{id} контроллера", required = true)
+//            @PathVariable("id") String controllerId,
+//            @ApiParam(value = "{device} устройство", required = true)
+//            @PathVariable("device") String deviceId,
+//      @ApiParam(value = "информация о подписке (ClientSubscription.class)", required = true)
+//      @RequestBody ClientSubscription entity
+  ) {
+
+    final List<ClientSubscription> clientSubscriptions = service.viewSubscription();
+
+    return new ResponseEntity<>(clientSubscriptions, okStatus);
+  }
+
     /***
       Subscribes client on event form device
      */
     @ApiOperation(value = "Подписаться на события", notes = "Подписаться на событие на устройстве {device} на контроллере {id}.", produces = "application/json", consumes = "application/json")
-    @RequestMapping(value = "/enable/{device}", method = RequestMethod.POST)
+    @RequestMapping(value = "/enable", method = RequestMethod.POST)
     public ResponseEntity<?> enable(
-            @ApiParam(value = "{id} контроллера", required = true)
-            @PathVariable("id") String controllerId,
-            @ApiParam(value = "{device} устройство", required = true)
-            @PathVariable("device") String deviceId,
+//            @ApiParam(value = "{id} контроллера", required = true)
+//            @PathVariable("id") String controllerId,
+//            @ApiParam(value = "{device} устройство", required = true)
+//            @PathVariable("device") String deviceId,
             @ApiParam(value = "информация о подписке (ClientSubscription.class)", required = true)
             @RequestBody ClientSubscription entity) {
 
-        log.debug("ControllerId={}, deviceId={}, subscription={}", controllerId, deviceId, entity);
+        log.debug("subscription={}", entity);
 
         service.addSubscription(entity);
 
@@ -40,16 +57,16 @@ public class SubscribeRestController {
     }
 
     @ApiOperation(value = "Обновить параметры подписки", notes = "Обновить параметры подписки на события на устройстве {device} на контроллере {id}.", produces = "application/json", consumes = "application/json")
-    @RequestMapping(value = "/update/{device}", method = RequestMethod.POST)
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
     public ResponseEntity<?> update(
-            @ApiParam(value = "{id} контроллера", required = true)
-            @PathVariable("id") String controllerId,
-            @ApiParam(value = "{device} устройство", required = true)
-            @PathVariable("device") String deviceId,
+//            @ApiParam(value = "{id} контроллера", required = true)
+//            @PathVariable("id") String controllerId,
+//            @ApiParam(value = "{device} устройство", required = true)
+//            @PathVariable("device") String deviceId,
             @ApiParam(value = "информация о подписке (ClientSubscription.class)", required = true)
             @RequestBody ClientSubscription entity) {
 
-        log.debug("ControllerId={}, deviceId={}, subscription={}", controllerId, deviceId, entity);
+      log.debug("subscription={}", entity);
 
         service.updateSubscription(entity);
 
@@ -58,7 +75,7 @@ public class SubscribeRestController {
 
 
     @ApiOperation(value = "Отписаться от всех событий на устройстве", notes = "Отписаться от всех событий на устройстве {device} на контроллере {id}.", produces = "application/json", consumes = "application/json")
-    @RequestMapping(value = "/disable/{device}", method = RequestMethod.GET)
+    @RequestMapping(value = "/disable/{device}", method = RequestMethod.DELETE)
     public ResponseEntity<?> disableDevice(
             @ApiParam(value = "{id} контроллера", required = true)
             @PathVariable("id") String controllerId,
@@ -73,7 +90,7 @@ public class SubscribeRestController {
     }
 
     @ApiOperation(value = "Отписаться от всех событий на контроллере", notes = "Отписаться от всех событий на контроллере {id}.", produces = "application/json", consumes = "application/json")
-    @RequestMapping(value = "/disableall", method = RequestMethod.GET)
+    @RequestMapping(value = "/disableall", method = RequestMethod.DELETE)
     public ResponseEntity<?> disableAll(
             @ApiParam(value = "{id} контроллера", required = true)
             @PathVariable("id") String controllerId) {
