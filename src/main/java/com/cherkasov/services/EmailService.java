7a673c8f1fb81481bfbc16590c857709a020cfa0
iridiumhawk <hawk.tmn@gmail.com>
2018-11-21 00:54:50 +0300

    deprecated

--- src/main/java/com/cherkasov/services/EmailService.java
+++ src/main/java/com/cherkasov/services/EmailService.java
@@ -2,23 +2,14 @@ package com.cherkasov.services;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@Deprecated
 public class EmailService {
-    @Autowired
-    private JavaMailSender emailSender;
 
-    public void sendSimpleMessage(String to, String subject, String text) {
-        log.debug("Sending mail");
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
-        emailSender.send(message);
-    }
+    @Autowired
+    public  JavaMailSender EMAIL_SENDER;
 }
--- src/main/java/com/cherkasov/services/SMSService.java
+++ src/main/java/com/cherkasov/services/SMSService.java
@@ -1,9 +1,17 @@
 package com.cherkasov.services;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@Deprecated
 public class SMSService {
+
+    @Value("${channels.sms.apikey}")
+    public String API_KEY_SMS;
+    @Value("${channels.sms.pattern}")
+    public String PATTERN_SMS;
+
 }
